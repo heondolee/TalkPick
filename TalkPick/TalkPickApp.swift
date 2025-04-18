135a274ac59447703b82e3a96b4dbd30f76e43b6
rundo <rundo999@gmial.com>
2025-04-18 15:36:48 +0900

    ModelContainer 연결 (앱 진입점에서)

--- TalkPick/TalkPickApp.swift
+++ TalkPick/TalkPickApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct TalkPickApp: App {
@@ -14,5 +15,7 @@ struct TalkPickApp: App {
             ContentView()
                 .preferredColorScheme(.light) // 👉 라이트 모드 고정
         }
+        .modelContainer(for: [User.self, DetailedCard.self]) // SwiftUI 앱에서는 @main struct에서 .modelContainer(for:)를 사용해 모델을 연결해야 합니다.
+        // 이걸 해줘야 SwiftUI 환경에 @Query, @Environment(\.modelContext) 같은 기능이 작동합니다.
     }
 }
