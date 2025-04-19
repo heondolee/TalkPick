a6b38e13d7691b2d56dc8c454ee8147cedab3eba
rundo <rundo999@gmial.com>
2025-04-19 14:32:29 +0900

    코드 이해 + 리팩토링

---
+++ TalkPick/Views/IconCardView.swift
@@ -0,0 +1,18 @@
+//
+//  IconCardView.swift
+//  TalkPick
+//
+//  Created by rundo on 4/19/25.
+//
+
+import SwiftUI
+
+struct IconCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconCardView()
+}
---
+++ TalkPick/Views/UserViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  UserViewModel.swift
+//  TalkPick
+//
+//  Created by rundo on 4/19/25.
+//
+
+import SwiftUI
+
+struct UserViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserViewModel()
+}
