db33c65fe297991fea7f88be90731c3e5d9e1a27
rundo <rundo999@gmial.com>
2025-04-22 20:21:14 +0900

    로그인 띄우기 성공

--- TalkPick/TalkPickApp.swift
+++ TalkPick/TalkPickApp.swift
@@ -17,7 +17,8 @@ struct TalkPickApp: App {
     
     var body: some Scene {
         WindowGroup {
-            TabBarView()
+//            TabBarView()
+            LoginView()
                 .preferredColorScheme(.light) // 👉 라이트 모드 고정
         }
         .modelContainer(for: [User.self, Card.self]) // SwiftUI 앱에서는 @main struct에서 .modelContainer(for:)를 사용해 모델을 연결해야 합니다.
